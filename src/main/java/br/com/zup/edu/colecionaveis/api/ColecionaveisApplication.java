8c3532794e8fdb54d2010d9caa4e3db68ed3777c
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-05 20:36:56 -0300

    Commit inicial.

---
+++ src/main/java/br/com/zup/edu/colecionaveis/api/ColecionaveisApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.colecionaveis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColecionaveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColecionaveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/colecionaveis/api/ColecionaveisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.colecionaveis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColecionaveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
